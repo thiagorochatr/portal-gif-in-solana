15580d253177f59b37a5b365424aa7f02bc710dd
thiagorochatr <thiagord2016@gmail.com>
2022-10-28 12:32:12 -0300

    Section 2 - Part 3 finished

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -4,11 +4,43 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod myepicproject {
-  use super::*;
-  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
-    Ok(())
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+        // Obtem a referência da conta
+        let base_account = &mut ctx.accounts.base_account;
+        // Inicializa o total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+    
+    pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+        // Obtem a referencia para a conta e incrementa total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
   }
 }
 
+// Anexa algumas variaveis ao contexto do StartStuffOff.
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Especifica que dados queremos no Contexto AddGif
+// Obtendo um controle sobre o fluxo das coisas 😊?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// Fala para a Solana o que queremos guardar nessa conta.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -1,13 +1,47 @@
 const anchor = require('@project-serum/anchor');
 
+// Precisa do programa do sistema, falaremos sobre isso em breve.
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Iniciando testes...")
 
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // Crie e defina o provedor. Nós o configuramos antes, mas precisávamos atualizá-lo, para que ele pudesse se comunicar com nosso frontend!
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+	
+  // Crie um par de chaves de conta para nosso programa usar.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Chame start_stuff_off, passe os parâmetros necessários!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Sua assinatura de transação", tx);
+
+  // Obtem dados da conta.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count:', account.totalGifs.toString())
+
+	
+  // Chama add_gif!
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  
+  // Obtem a conta novamente e veja o que mudou.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count:', account.totalGifs.toString())
 }
 
 const runMain = async () => {
